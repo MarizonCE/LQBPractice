35d04195c680c93e0683b5f2322e1e33261980e1
MarizonCE <Marizon20250212@gmail.com>
2025-03-14 11:29:11 +0800

    Update 549--扫雷.py

--- 基础算法/枚举/549--扫雷.py
+++ 基础算法/枚举/549--扫雷.py
@@ -1,27 +1,27 @@
-import os
-import sys
-
-
-# 请在此输入您的代码
 def input_list():
+    # 读取一行输入，并将其转换为整数列表
     return list(map(int, input().split()))
 
 
+# 输入列表的行数 n 和列数 m
 n, m = input_list()
-a = []
-for i in range(n):
-    a.append(input_list())
-b = [[0] * m for _ in range(n)]
+# 输入n行数据，存入列表a
+a = [input_list() for _ in range(n)]
 
+# 初始化结果列表，初始值为0
+b = [[0] * m for _ in range(n)]
+# 定义8个可能的相邻方向（左上、上、右上、左、右、左下、下、右下）
 dire = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+# 遍历列表中的每个元素
 for i in range(n):
     for j in range(m):
         if a[i][j] == 1:
-            b[i][j] = 9
+            b[i][j] = 9  # 当前位置有地雷，设为9
         else:
-            for k in range(8):
-                x, y = i + dire[k][0], j + dire[k][1]
+            for dx, dy in dire:  # 遍历8个方向
+                x, y = i + dx, j + dy
                 if 0 <= x < n and 0 <= y < m:
-                    b[i][j] += a[x][y]
-        print(b[i][j], end=' ')
-    print()
+                    b[i][j] += a[x][y]  # 计算周围地雷数
+        print(b[i][j], end=' ')  # 按行输出
+    print()  # 每行结束后换行
