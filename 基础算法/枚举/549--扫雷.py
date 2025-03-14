0c744e47659ad825c65096cc44657ed06c937269
MarizonCE <Marizon20250212@gmail.com>
2025-03-14 11:54:23 +0800

    更新一题

--- 基础算法/枚举/549--扫雷.py
+++ 基础算法/枚举/549--扫雷.py
@@ -6,22 +6,22 @@ def input_list():
 # 输入列表的行数 n 和列数 m
 n, m = input_list()
 # 输入n行数据，存入列表a
-a = [input_list() for _ in range(n)]
+a = [input_list() for _ in range(n)]  # 注意函数的调用方式
 
 # 初始化结果列表，初始值为0
-b = [[0] * m for _ in range(n)]
+b = [[0] * m for _ in range(n)]  # 注意为[0]而不是[]
 # 定义8个可能的相邻方向（左上、上、右上、左、右、左下、下、右下）
 dire = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
 # 遍历列表中的每个元素
 for i in range(n):
     for j in range(m):
-        if a[i][j] == 1:
+        if a[i][j] == 1:  # 注意等号比较的写法
             b[i][j] = 9  # 当前位置有地雷，设为9
         else:
             for dx, dy in dire:  # 遍历8个方向
                 x, y = i + dx, j + dy
                 if 0 <= x < n and 0 <= y < m:
-                    b[i][j] += a[x][y]  # 计算周围地雷数
+                    b[i][j] += a[x][y]  # 计算周围地雷数  # 注意是b[i][j]这种形式，而不是b[i,j]
         print(b[i][j], end=' ')  # 按行输出
     print()  # 每行结束后换行
